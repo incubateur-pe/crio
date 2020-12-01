d034ea7343e857811cf7dcf9be14ea45eb14e9a7
ulrich giraud <ulrich.giraud1@gmail.com>
2020-12-01 20:00:53 +0100

    build(ci): initialize gitlab ci

---
+++ molecule/common/env_ugi.sh
@@ -0,0 +1,1 @@
+CRIO_REPOSITORY_BASE_URL=http://10.0.4.40:8081/repository/download-opensuse/
--- molecule/common/env_ugi_distant.sh
+++ molecule/common/env_ugi_distant.sh
@@ -1,3 +1,4 @@
+CRIO_REPOSITORY_BASE_URL=http://10.0.4.40:8081/repository/download-opensuse/
 REMOTE_HOST=true
 PROVIDER_ARGS=['host="kvm"', 'username="libvirt-user"']
 SSH_CONFIG=ssh_config_ugi
