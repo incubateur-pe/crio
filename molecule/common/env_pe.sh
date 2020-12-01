1f40d5a4492788995b48908411475fc69e3b3df9
ulrich giraud <ulrich.giraud1@gmail.com>
2020-12-01 20:00:53 +0100

    build(ci): initialize gitlab ci

--- molecule/common/env_pe.sh
+++ molecule/common/env_pe.sh
@@ -1,3 +1,4 @@
+CRIO_REPOSITORY_BASE_URL=https://download.opensuse.org
 http_proxy
 https_proxy
 HTTP_PROXY
--- molecule/common/env_pe_distant.sh
+++ molecule/common/env_pe_distant.sh
@@ -1,3 +1,4 @@
+CRIO_REPOSITORY_BASE_URL=https://download.opensuse.org
 REMOTE_HOST=true
 PROVIDER_ARGS=['host="iugi3000-HP-ProDesk-400-G4-MT.dgasi.pole-emploi.intra"', 'username="libvirt-user"']
 SSH_CONFIG=ssh_config_pe
